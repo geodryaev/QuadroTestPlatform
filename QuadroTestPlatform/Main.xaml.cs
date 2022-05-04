06b246c6e253023f7c98857a9b2c5fc9dd726813
GoodBoy <GoodBoy@GOODBOY>
2022-05-04 16:01:56 +0300

    Добавьте файлы проекта.

---
+++ QuadroTestPlatform/Main.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace QuadroTestPlatform
+{
+
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+            TemsTree.Items.Add("asd");
+            TemsTree.Items.Add("asd2");
+            TemsTree.Items.Add("asd3");
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
