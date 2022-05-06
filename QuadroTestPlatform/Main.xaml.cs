4a0ccb7c20eb152d0c72cf7d59761a6125053d23
Good Boy <geodryaev1@gmail.com>
2022-05-06 12:25:45 +0300

    Sql startr

--- QuadroTestPlatform/Main.xaml.cs
+++ QuadroTestPlatform/Main.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
+using System.Data.SqlClient;
 
 namespace QuadroTestPlatform
 {
@@ -9,6 +9,7 @@ namespace QuadroTestPlatform
         public Main()
         {
             InitializeComponent();
+            tb_pathSQL.Text = Properties.Settings.Default.pathSQL;
             TemsTree.Items.Add("asd2");
             TemsTree.Items.Add("asd3");
         }
@@ -21,10 +22,34 @@ namespace QuadroTestPlatform
             }
         }
 
-
         private void b_deleteTems_Click(object sender, RoutedEventArgs e)
         {
             TemsTree.Items.Remove(TemsTree.SelectedItem);
+        }
+
+        private void b_replacePathSQL_Click(object sender, RoutedEventArgs e)
+        {
+            if (tb_pathSQL.Text.ToString() != null && tb_pathSQL.Text.ToString() != "")
+            {
+                Properties.Settings.Default.pathSQL = tb_pathSQL.Text.ToString();
+                Properties.Settings.Default.Save();
+                MessageBox.Show("Настройки сохранены");
+            }
+        }
+
+        public string strSQLConnection()
+        {
+            return "Server=" + Properties.Settings.Default.pathSQL + ";Initial Catalog =QTPDB; User ID = sa; Password = qwerty12";
+        }
+
+        private void b_createTableDB_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection connect = new SqlConnection(strSQLConnection()))
+            {
+                connect.Open();
+
+                connect.Close();
+            }
         }
     }
 }
